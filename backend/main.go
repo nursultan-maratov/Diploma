d447d53172c24f02a817cf22303e4bb04566c8bc
Zhanbolat Maratov <admin@halyktravel.kz>
2024-10-08 18:44:40 +0500

    feat: added echo library and up server

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 	"github.com/nursultan-maratov/Diploma.git/internal/postgres"
 	"log"
+	"net/http"
 )
 
 func main() {
 
 	_, err := postgres.ConnectDefaultDataBase()
 	if err != nil {
-		log.Fatalf("Kaput db is not workings %v", err)
+		log.Fatalf("Kaput db is not working %v", err)
 	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":80"))
 
 }
